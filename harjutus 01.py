f9eb1fd1d1b205e62e87188d12056ab2c0341413
kviilma-tech <kviilma@hkhk.edu.ee>
2025-11-10 10:35:09 +0200

    Add files via upload

---
+++ harjutus 01.py
@@ -0,0 +1,28 @@
+# print ("Tere \"suur\" maailm!")
+# Kaspar Viilma
+# Harjutus 1
+import turtle
+
+turtle.speed(0)
+#kolmnurk
+turtle.up()
+turtle.goto(-400,300)
+turtle.down()
+for i in range(3):
+    turtle.forward(200)
+    turtle.right(120)
+  
+#nimetäht K
+turtle.penup()
+turtle.setpos(50,50)
+turtle.goto(-100,100)
+turtle.pendown()
+turtle.pensize(40)   
+turtle.right(90)
+turtle.forward(200)
+turtle.backward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.backward(100)
+turtle.left(80)
+turtle.forward(100)
