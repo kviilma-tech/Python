abb2cd9e381c78d3c0c08c4ea500e971dae4c000
kviilma-tech <kviilma@hkhk.edu.ee>
2026-02-22 18:47:27 +0200

    Python

---
+++ Iseseisev töö kujund 6.py
@@ -0,0 +1,40 @@
+import turtle
+ 
+t = turtle.Turtle()
+
+# Ristkülik 1
+t.fd(100)
+t.rt(90)
+t.fd(300)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(300)
+t.rt(90)
+
+# Ristkülik 2
+t.backward(400)
+t.rt(90)
+t.fd(100)
+t.lt(90)
+t.fd(400)
+t.lt(90)
+t.fd(100)
+
+# Ristkülik 3
+t.fd(300)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(300)
+t.lt(90)
+t.fd(100)
+
+# Ristkülik 4
+t.fd(400)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(400)
+t.lt(90)
+t.fd(100)
