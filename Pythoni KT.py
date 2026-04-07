573057cc15375fb429113d847ddd7a23e9b5ae78
kviilma-tech <kviilma@hkhk.edu.ee>
2026-04-07 12:33:25 +0300

    Python KT

--- Pythoni KT.py
+++ Pythoni KT.py
@@ -1,10 +1,9 @@
 import requests
 
-<<<<<<< HEAD
 with open("posts.json", "r", encoding="utf-8") as f:
     data = json.load(f)
     posts = data["posts"]
-=======
+
 URL = "https://dummyjson.com/posts"
 r = requests.get(URL)
 data = r.json()
@@ -13,7 +12,6 @@ posts = data["posts"]
 max_post = posts[0]
 views_sum = 0
 reactions_sum = 0
->>>>>>> eb4c1ec53b7396920d32243e679bd658e331e1c5
 
 for post in posts:
     title = post['title']
